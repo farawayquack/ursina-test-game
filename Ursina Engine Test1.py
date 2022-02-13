7cb16696b9c0bb2d43d1d7981882515b092618b4
Robert <99143750+farawayquack@users.noreply.github.com>
2022-02-13 12:05:29 +0100

    Add files via upload

---
+++ Ursina Engine Test1.py
@@ -0,0 +1,48 @@
+from ursina import *
+from ursina import *
+
+class Test_cube(Entity):
+    def __init__(self):
+        super().__init__(
+            model = 'cube',
+            color = color.white,
+            texture = 'white_cube',
+            rotation = Vec3(45, 45, 45)
+            )
+
+class Test_button(Button):
+    def __init__(self):
+        super().__init__(
+            parent = scene,
+            model = 'cube',
+            texture = 'brick',
+            color = color.blue,
+            highlight_color = color.red,
+            pressed_color = color.pink
+        )
+
+    def input(self, key):
+        if self.hovered:
+            if key == 'left mouse down':
+                print('button pressed')
+
+def update():
+    if held_keys['a']:
+        test1.x -= 4 * time.dt
+    if held_keys['s']:
+        test1.y -= 4 * time.dt
+    if held_keys['d']:
+        test1.x += 4 * time.dt
+    if held_keys['w']:
+        test1.y += 4 * time.dt
+
+app = Ursina()
+
+test1 = Entity(model = 'quad', color = color.red, scale = (1, 3), position = (1,5))
+
+sans_texture = load_texture('Assets/mcsans.png')
+sans = Entity(model = 'quad',texture = sans_texture)
+
+test_cube = Test_button()
+
+app.run()
